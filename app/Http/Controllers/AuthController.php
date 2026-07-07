88eb674279d62943f420a52e8586db3a64f8a7e2
Finnn-45 <arfinsmktibazma@gmail.com>
2026-07-07 10:57:14 +0700

    checkpoint

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -8,7 +8,6 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Password;
 use Illuminate\Auth\Events\Registered;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Facades\Http;
